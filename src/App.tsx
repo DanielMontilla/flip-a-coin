9e9cc97d2f8cb87e5b2613828c4fd6c7669eaa2d
Daniel Montilla <montilla.d2000@gmail.com>
2021-12-01 18:19:16 -0400

    core function done!

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,55 @@
 import React from 'react';
-import { setInterval } from 'timers';
 import './App.css';
+import { rand, randPick } from './util';
 
-// import { randInt } from './util';
+type color = 'red' | 'blue'
 
 interface props {};
 interface state {
-  state: boolean,
-  color: string | undefined
-  animations: Record<`flip` | 'toss', React.CSSProperties | undefined>
+  color: color,
+  animations: Record<`flip` | 'toss', React.CSSProperties | undefined>,
+  flipping: boolean
 }
 
 export default class App extends React.Component<props, state> {
 
   public state: state = {
-    state: true,
-    color: undefined,
+    color: randPick(['red', 'blue']),
     animations: {
       flip: undefined,
       toss: undefined
-    }
+    },
+    flipping: false
   }
 
-  public history: string[] = [];
+  public history: color[] = [];
 
   public render() {
     let { color } = this.state;
     let { toss, flip } = this.state.animations;
 
     return <div className="App">
-      <div className="container">
+      <div className="container" onClick={ () => { this.flip() }}>
         <div style={ toss } onAnimationEnd={ () => { this.resetAnims() } }>
           <div style={ flip }>
               <div className={`coin`} style={{ backgroundColor: color }}/>
           </div>
         </div>
-        <button onClick={ () => { this.flip() }} disabled={(toss !== undefined)}> hi </button>
       </div>
     </div>
   }
 
   private flip() {
-    // let result = (rand() > .5);
-    let { toss, flip, details } = this.generateAnims();
+    if (this.state.flipping) return;
+    this.setState({ flipping: true });
+
+    let change = (rand() > .5);
+
+    let { toss, flip, details } = this.generateAnims(change);
     let { count, interval } = details;
+
     this.setState({ animations: { toss: toss, flip: flip } } );
     this.waitAndChange(interval, count, true);
-    
   }
 
   private toggleColor () {
@@ -58,20 +61,20 @@ export default class App extends React.Component<props, state> {
     }
   }
 
-  private waitAndChange(amount: number, count: number, f: boolean = false) {
-    if (count === 0) return;
+  private waitAndChange(amount: number, count: number, first: boolean = false) {
+    if (count === 0) { setTimeout( () => { this.setState({ flipping: false }) }, 100); return };
     count--;
     setTimeout( () => {
       this.toggleColor();
       this.waitAndChange(amount, count);
-    }, (f) ? amount / 2 : amount )
+    }, (first) ? amount / 2 : amount )
   }
 
   private resetAnims() {
     this.setState({ animations: { flip: undefined, toss: undefined } });
   }
 
-  private generateAnims(): { 
+  private generateAnims(change: boolean): { 
     flip: React.CSSProperties,
     toss: React.CSSProperties, 
     details: {
@@ -79,12 +82,18 @@ export default class App extends React.Component<props, state> {
       count: number
     } 
   } {
+    let odds = [ 3, 5, 7 ];
+    let evens = [ 4, 6 ];
+
+    // change == true => flips = odd
+    // change == false => flips = even
+
+    let posabilities = (change) ? odds : evens;
 
     let tosses = 1;
-    let flips = 4;
-    let tossDuration = 1500;
-    let flipDuration = Math.round(tossDuration / flips) - .01*tossDuration;
-
+    let flips = randPick(posabilities);
+    let tossDuration = flips * 325;
+    let flipDuration = Math.round(tossDuration / flips);
  
     return {
        flip: {
@@ -97,7 +106,7 @@ export default class App extends React.Component<props, state> {
           animationName: 'toss',
           animationDuration: `${tossDuration}ms`,
           animationIterationCount: tosses,
-          animationTimingFunction: 'ease-in-out'
+          animationTimingFunction: 'ease-in'
        },
 
        details: {
